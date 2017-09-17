5deb920be4bc7f1a490e707853cbb3426f91059c
Victor <em3888@gmail.com>
2017-09-17 23:05:11 +0800

    Init

--- index.ios.js
+++ index.ios.js
@@ -17,7 +17,7 @@ export default class rnnativeasandbox extends Component {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
-          Welcome to React Native!
+          Is this working?, Probabaly
         </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js
