2e5636ab89a87b7ca78bd183bf1dff21a80635b6
Victor <em3888@gmail.com>
2017-09-27 18:18:01 +0800

    Working state

--- app/scenes/loginScene.js
+++ app/scenes/loginScene.js
@@ -5,7 +5,7 @@ import {
     View,
     Text
 } from 'native-base'
-import { Image } from 'react-native'
+import { Image, StyleSheet, Dimensions } from 'react-native'
 import Login from '../components/login'
 import { observer } from 'mobx-react/native'
 
@@ -14,21 +14,40 @@ export default class LoginScene extends Component {
     constructor(props) {
         super(props)
     }
-    render() {
-        const { theme, stores } = this.props
-        return (
-            <View>
-            <Container theme={theme}>
-                <View>
 
-                        <Image source={stores.settings.LoginBG}>
-                            <View>
+    render() {
+        const {theme, stores} = this.props
+        return (
+                <Container theme={theme}>
+                        <Image source={stores.settings.LoginBG} style={style.loginBackground}>
+                            <View style={style.loginForeground}>
                                 <Login {...this.props}/>
                             </View>
                         </Image>
-                </View>
-            </Container>
-            </View>
+                </Container>
         )
     }
 }
+
+const style = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0
+    },
+    loginBackground: {
+        flex:1,
+        width: null,
+        height: null
+    },
+    loginForeground: {
+        flex:1,
+        marginTop: Dimensions.get('window').height/1.75,
+        paddingTop: 20,
+        paddingLeft: 10,
+        paddingRight: 10,
+        paddingBottom: 90,
+        bottom: 0
+    }
+})
