61b98d72ba6dc45c89be7bd86b04ea6b348e33ed
Victor <em3888@gmail.com>
2017-10-03 00:40:39 +0800

    Add some postStore

--- app/stores/postStore.js
+++ app/stores/postStore.js
@@ -7,7 +7,7 @@ const Blob =RNFetchBlob.polyfill.Blob
 window.XMLHttpRequest = RNFetchBlob.polyfill.XMLHttpRequest
 window.Blob = Blob
 
-const base = 'posts'
+const base = 'posts' //base on the firebase node
 
 export default class Poststore extends MobxFirebaseStore {
     constructor() {
@@ -18,4 +18,25 @@ export default class Poststore extends MobxFirebaseStore {
         })
 
     }
+
+    subs(){
+        return [{
+            subKey: base,
+            asList: true,
+            path: base
+        }]
+    }
+
+    @action
+    add(text,url){
+        let post = {text:text, created:Date.now(),user: this.user.uid,url: url }
+        let key = this.fb.child(base).push().key //Update the post with a key to get back
+        
+        let updates = {}
+        update['/'+base+'/'+key] = post
+        update['/'+this.user.uid+'/history/'+key] = true
+        this.fb.update(updates) //Demo of update 2 different place
+    }
+
+
 }
